7d22a377e6afb69d1c4eae1189c9c3c8775539c4
Noer Paanakker <noer@healthyworkers.nl>
2017-09-06 17:23:13 +0200

    added timestamps for each reminder

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addReminder, deleteReminder } from '../actions'
+import { addReminder, deleteReminder } from '../actions';
+import moment from 'moment';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      text: ''
+      text: '',
+      dueDate: ''
     }
 
     this.addReminder = this.addReminder.bind(this);
@@ -15,18 +17,22 @@ class App extends Component {
     this.renderReminders = this.renderReminders.bind(this);
   }
 
+  //Creates new reminder with value the text put in input
   addReminder() {
+    console.log('this.state.dueDate', this.state.dueDate)
     if(this.state.text.length === 0) {
       alert('Please put in an action!!')
     } else {
-      this.props.addReminder(this.state.text);
+      this.props.addReminder(this.state.text, this.state.dueDate);
     }
   }
 
+  //deletes reminder with certain id
   deleteReminder(id) {
     this.props.deleteReminder(id);
   }
 
+  //renders each reminder with unique key
   renderReminders() {
     const { reminders } = this.props;
     return (
@@ -35,7 +41,7 @@ class App extends Component {
         reminders.map(reminder => {
         return (
               <li key={reminder.id} className='list-group-item'>
-                <div className="list-item">{reminder.text}</div>
+                <div className="list-item">{reminder.text} <em>{moment(new Date (reminder.dueDate)).fromNow()}</em></div>
                 <div onClick={() => this.deleteReminder(reminder.id)} className="list-item delete-button">&#x2715;</div>
               </li>
             )
@@ -52,6 +58,7 @@ class App extends Component {
         <div className="form-inline">
         	<div className="form-group">
         		<input onChange={event => this.setState({ text: event.target.value })} type="text" className="form-control" placeholder='I have to...' required/>
+            <input onChange={event => this.setState({ dueDate: event.target.value })} type="datetime-local" className="form-control"/>
             <br />
             <button onClick={() => this.addReminder()} className="btn btn-success">Add a Reminder</button>
         	</div>

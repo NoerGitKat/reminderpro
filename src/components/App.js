365c648a257c992dbc31082efcfb0b50a51b71f1
Noer Paanakker <noer@healthyworkers.nl>
2017-09-06 16:31:03 +0200

    added button funcionality to add reminder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addReminder } from '../actions'
-import { bindActionCreators } from 'redux';
+import { addReminder, deleteReminder } from '../actions'
 
 class App extends Component {
   constructor(props) {
@@ -10,10 +9,41 @@ class App extends Component {
     this.state = {
       text: ''
     }
+
+    this.addReminder = this.addReminder.bind(this);
+    this.deleteReminder = this.deleteReminder.bind(this);
+    this.renderReminders = this.renderReminders.bind(this);
   }
 
   addReminder() {
-    console.log('oh wowie!')
+    if(this.state.text.length === 0) {
+      alert('Please put in an action!!')
+    } else {
+      this.props.addReminder(this.state.text);
+    }
+  }
+
+  deleteReminder(id) {
+    console.log('deleting in app id', id);
+    console.log('lemme see this.props', this.props)
+  }
+
+  renderReminders() {
+    const { reminders } = this.props;
+    return (
+      <ul className="list-group col-sm-4">
+      {
+        reminders.map(reminder => {
+        return (
+              <li key={reminder.id} className='list-group-item'>
+                <div className="list-item">{reminder.text}</div>
+                <div onClick={() => this.deleteReminder(reminder.id)} className="list-item delete-button">&#x2715;</div>
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
   }
 
   render() {
@@ -22,17 +52,21 @@ class App extends Component {
         <h1>This is Reminder Pro!</h1>
         <div className="form-inline">
         	<div className="form-group">
-        		<input onChange={event => this.setState({ text: event.target.value })} type="text" className="form-control" placeholder='I have to...'/>
-        		<button onClick={() => this.addReminder()} className="btn btn-success">Add a Reminder</button>
+        		<input onChange={event => this.setState({ text: event.target.value })} type="text" className="form-control" placeholder='I have to...' required/>
+            <br />
+            <button onClick={() => this.addReminder()} className="btn btn-success">Add a Reminder</button>
         	</div>
         </div>
+        { this.renderReminders() }
       </div>
-    );
+    )
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({addReminder}, dispatch);
+function mapStateToProps(state) {
+  return {
+    reminders: state
+  }
 }
 
-export default App;
+export default connect(mapStateToProps, { addReminder })(App);
